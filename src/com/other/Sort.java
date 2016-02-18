ef4c92860144129aaadd72a4d1265503e4bd4597
lizx <lizx@ihczd.com>
2016-02-18 10:27:38 +0800

    2016-02-18 lizx 增加二分法插入排序
    Change-Id: I5eecd22d58f4c978df4df87cd66b4f6a3d016685
    Signed-off-by: lizx <lizx@ihczd.com>

--- src/com/other/Sort.java
+++ src/com/other/Sort.java
@@ -8,11 +8,8 @@ package com.other;
  * @date 2016-2-17 下午4:21:32
  */
 public class Sort {
-	//38 49 65 76 97 13
-	//38 49 65 76 13 97
-	int[] data = {49,38,65,97,76,13,27,49,78,34,12,64,1};
 	
-	public void 直接插入排序(){
+	public void 直接插入排序(int[] data){
 		for(int i = 1;i < data.length;i++){
 			int temp = data[i];
 			int j;
@@ -23,16 +20,45 @@ public class Sort {
 					break;
 				}
 			}
-			data[j] = temp;
+			data[j+1] = temp;
 		}
-		
+	}
+	public void 二分法插入排序(int[] data){
+		int z = 0;
+		for(int i = 1;i < data.length;i++){
+			int low = 0;//低位下标
+			int temp = data[i];
+			int hight = i-1;
+			if(temp > data[hight]){
+				continue;
+			}
+			while(low < hight){
+				int mid = (hight+low)/2;//二分位下标
+				
+				if(temp < data[mid]){//在左边
+					hight = mid - 1;
+				}else{//在右边
+					low = mid + 1;
+				}
+				z++;
+			}
+			for(int j = i;j > low;j--){
+				data[j] = data[j-1];
+			}
+			if(low != i){
+				data[low] = temp;
+			}
+		}
+		System.out.println("计算次数："+z);
+	}
+	//1 3 4 2 5 6
+	public static void main(String[] args) {
+		int[] data = {49,38,65,97,76,13,27,49,78,34,12,64,1};
+		Sort sort = new Sort();
+		//sort.直接插入排序(data);
+		sort.二分法插入排序(data);
 		for(int i = 0;i < data.length; i++){
 			System.out.print(data[i]+" ");
 		}
-	}
-	public static void main(String[] args) {
-		
-		Sort sort = new Sort();
-		sort.直接插入排序();
 	}
 }
